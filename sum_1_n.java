ce8aeb3cab0049e2f9bccd049a3000eb400f5be5
BIDHAN99 <bidhansingh9931@gmail.com>
2025-09-01 21:21:57 +0530

    sum_1_n ten_natural_numbers

---
+++ sum_1_n.java
@@ -0,0 +1,11 @@
+public class sum_1_n {
+    public static void main(String[] args) {
+        int n = 50;
+        int sum = 0;    
+    for(int i=1;i<=n;i++)
+    {
+        sum = sum + i;
+    }
+    System.out.println("sum of "+n+" natural number is "+sum);
+}
+}
---
+++ ten_natural_numbers.java
@@ -0,0 +1,8 @@
+public class ten_natural_numbers {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
